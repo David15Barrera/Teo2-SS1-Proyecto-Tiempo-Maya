c283107619b49c32c45c9f272191e40bbce7c463
David15Barrera <david1315barrera@gmail.com>
2024-05-19 11:30:44 -0600

    Modificacion en cuenta Larga

--- Web/cuentaLarga.php
+++ Web/cuentaLarga.php
@@ -1,5 +1,6 @@
-<?php session_start(); ?>
 <?php 
+session_start();
+
 $conn = include "conexion/conexion.php";
 
 if(isset($_GET['fecha'])){
@@ -7,6 +8,23 @@ if(isset($_GET['fecha'])){
 }else{
     date_default_timezone_set('US/Central');  
     $fecha_consultar = date("Y-m-d");
+}
+
+// Consultar la información de la tabla `pagina`
+$query = "SELECT htmlCodigo FROM pagina WHERE categoria = 'Cuenta larga'";
+$query2 = "SELECT htmlCodigo FROM pagina WHERE nombre='PasosCuentaLarga'";
+
+$result = $conn->query($query);
+$result2 = $conn->query($query2);
+
+if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $htmlCodigo = $row['htmlCodigo'];
+}
+
+if ($result2 && $result2->num_rows > 0) {
+    $row2 = $result2->fetch_assoc();
+    $htmlCodigoPasos = $row2['htmlCodigo'];
 }
 ?>
 <!DOCTYPE html>
@@ -23,12 +41,28 @@ if(isset($_GET['fecha'])){
     <?php include "NavBar.php" ?>
     <div>
         <section id="inicio" class="inicio">
-            <div id="inicioContainer" class="inicio-container">
-                <div id='formulario'>
+            <div id="inicioContainer" class="inicio-container"> <br><br><br><br>
+            <?php echo "<h1> Cuenta Larga Maya </h1>";?> <br>
+            <a href='#informacion' class='btn-get-started'>Informacion</a>
+            <a href='#calculadora' class='btn-get-started'>Calculadora</a>
+            <a href='#funcionamiento' class='btn-get-started'>Funcionamiento</a>
+                <br><br><br><br>
+            </div>
+        </section> 
+    </div>
+    <?php
+        $stringPrint = "<section class='inicio' id='informacion'> <div class='container'> <div class='section-header'><h3 class='section-title'> Informacion </h3> </div>";
+        $stringPrint .= "</div> </section> <hr>";
+        $stringPrint .= "<div class='recuperado'>" . $htmlCodigo . "</div>";  // Aquí se inserta el contenido de la tabla `pagina` con la clase CSS;  // Aquí se inserta el contenido de la tabla `pagina`
+
+    echo $stringPrint;
+    ?>
+        <h1  id= "calculadora">Calculadora</h1>
+               <div id="formulario">
                     <div class="container">
                         <h2>"Cuenta Larga" Maya a Gregoriano</h2>
                         <div class="section">
-                            <h2>Calendario Gregoriano</h2>
+                            <h3>Calendario Gregoriano</h3>
                             <label for="day">Día:</label>
                             <input type="number" id="day" min="1" max="31" value="2" maxlength="2">
                             <label for="month">Mes:</label>
@@ -44,12 +78,12 @@ if(isset($_GET['fecha'])){
                             </div>
                         </div>
                         <div class="section">
-                            <h2>Días Julianos</h2>
+                            <h3>Días Julianos</h3>
                             <input type="number" id="julian-day" value="2458575.51412037">
                             <button onclick="convertJulian()">Convertir</button>
                         </div>
                         <div class="section">
-                            <h2>Cuenta Larga Maya</h2>
+                            <h3>Cuenta Larga Maya</h3>
                             <div class="table-container">
                                 <table>
                                     <tr>
@@ -83,22 +117,29 @@ if(isset($_GET['fecha'])){
                                         <td id="wf-haab"></td>
                                     </tr>
                                 </table>
+                                <br>
                             </div>
                         </div>
                     </div>
                 </div>
-                <br>
-    <br>
-    <br>
-<br>
+        <section id="funcionamiento">
+            <div class="container">
+                <div class="row about-container">
+                    <div class="section-header">
+                        <h3 class="section-title">¿Como funciona?</h3>
+                    </div>
+                    <div class="content-step">
+                        <?php
+                        // Corrección: asigna directamente el contenido de $htmlCodigoPasos a $stringPrint
+                        $stringPrint = "<div class='recuperado'>" . $htmlCodigoPasos . "</div>";
+                        echo $stringPrint;
+                        ?>
+                    </div>
+                </div>
             </div>
-        </section> 
-    </div>
-    <?php
-    $stringPrint = "<section class='inicio'> <div class='container'> <div class='section-header'><h3 class='section-title'> Informacion </h3> </div>";
-    $stringPrint .= "</div> </section> <hr>";
-    echo $stringPrint;
-    ?>
+        </section>
+
+
     <?php include "blocks/bloquesJs1.html" ?>
     <script src="js/cuentaLarga.js"></script>
     <script src="js/cambioFondo.js"></script>
